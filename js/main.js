0bac71b29d575553f399d8070dd4b55ff1e733fe
Karina Boronina <Gubashina@yandex.ru>
2023-03-15 13:48:17 +0300

    add animate first title

--- js/main.js
+++ js/main.js
@@ -27,7 +27,8 @@
     if (window.innerWidth <= 720) {
         for (let link of menuLinks) {
             link.addEventListener('click', () => {
-                headerMenu.classList.remove('header__menu_active');
+                headerMenu.classList.remove('header__menu_active'); 
+                // console.log(link);
             })
         }
     }
@@ -80,3 +81,41 @@
     });
 
 }());
+
+// Animations
+
+    const animItems = document.querySelectorAll('.anim_items');
+
+    if (animItems.length > 0) {
+        window.addEventListener('scroll', animationOnScroll);
+
+        function animationOnScroll() {
+            for(let i = 0 ; i < animItems.length; i++) {
+                const animItem = animItems[i];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+
+                if ((animItemHeight > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('_active');
+                } else {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+        animationOnScroll();
+    }
+
+    
